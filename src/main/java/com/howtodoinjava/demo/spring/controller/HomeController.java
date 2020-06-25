41f78bb664d0e1685001d68c86b99f04c85b8561
rajanna.p <prgarani@gmail.com>
2020-06-25 19:26:35 +0530

    Test changes

--- src/main/java/com/howtodoinjava/demo/spring/controller/HomeController.java
+++ src/main/java/com/howtodoinjava/demo/spring/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
+//comment for test
 //@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller
 public class HomeController 
